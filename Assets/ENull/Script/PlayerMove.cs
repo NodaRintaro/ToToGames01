281b9c8c60fd2a99d7cb1ae60d687c90fbc00844
Enulu <enuarle8@icloud.com>
2025-01-04 19:08:32 +0900

    [Add]Playerかりおき

---
+++ Assets/ENull/Script/PlayerMove.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Grid _grid;
+
+    [SerializeField] Tilemap _tilemap;
+
+
+
+    private void PlayerMove()
+    {
+
+    }
+}
