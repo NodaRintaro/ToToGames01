87eec39b15daf5694dcc2de2b60e0d28c1ca70d3
Enulu <enuarle8@gmail.com>
2025-04-09 01:10:13 +0900

    [fix]歩行方法の変更

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField,Header("���s���x")] int _walkSpeed = 10;
+    [SerializeField,Header("歩行速度")] float _walkSpeed = 10;
 
     private Vector2 _dir = default;
     
     private Rigidbody2D _rb;
 
     private float _x, _y;
+
+    private float _timer;
 
     private void Start()
     {
@@ -21,9 +23,14 @@ public class PlayerMove : MonoBehaviour
     {
         _x = Input.GetAxisRaw("Horizontal");
         _y = Input.GetAxisRaw("Vertical");
-        
-        _dir = new Vector2(_x,_y);
 
-        _rb.linearVelocity = _dir * _walkSpeed;
+        _timer += Time.deltaTime;
+        
+        if(_timer > _walkSpeed)
+        {
+            this.transform.position = new Vector2(this.transform.position.x + _x, this.transform.position.y + _y);
+            if(_x == 0 || _y == 0)
+                _timer = 0;
+        }
     }
 }
